89a7cd21c2a0fab8e9ac9937c698b3731719e9eb
Louis Rémus <15720130+louis-r@users.noreply.github.com>
2017-12-08 22:40:51 -0800

    Plotting heatmap to see what normalizing gives

--- mnist/fgsm_mnist.py
+++ mnist/fgsm_mnist.py
@@ -9,14 +9,16 @@ import torch
 from torchvision import datasets, transforms
 from torch.autograd import Variable
 import numpy as np
-# noinspection PyPackageRequirements
-from mnist_torch import Net
+# noinspection PyUnresolvedReferences
+from mnist_torch import BaseNet
 
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Add src path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from src.fgsm import get_label, run_targeted_attack, run_non_targeted_attack, image_to_tensor, tensor_to_image, \
+from src.fgsm import plot_heatmap_results, get_label, run_targeted_attack, run_non_targeted_attack, image_to_tensor, \
+    tensor_to_image, \
     draw_result, \
     random_noise
 
@@ -28,29 +30,28 @@ if __name__ == '__main__':
     #     'step_size': 0.1
     # }
     # Load our trained model
-    model = Net()
-    model.load_state_dict(torch.load('mnist_pytorch_R_normalized.pt'))
+    model = BaseNet()
+    model.load_state_dict(torch.load('mnist_basenet_training_normalized.pt'))
 
     # MNIST data loader
     test_loader = torch.utils.data.DataLoader(
         datasets.MNIST('data', train=False, transform=transforms.Compose([
             transforms.ToTensor(),
-            # transforms.Normalize((0.1307,), (0.3081,))
+            transforms.Normalize((0.1307,), (0.3081,))
         ])),
         batch_size=1, shuffle=True)
 
     final_losses = []
-    eps_range = np.arange(1, 100, 10) / 225.
-    step_size_range = [0.1, 0.05, 0.001, 0.0005, 0.00001]
+    eps_range = [5 * i / 200 for i in range(1, 11)]
+    step_size_range = [0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001, 0.00005, 0.00001]
     res = np.zeros(shape=(len(eps_range), len(step_size_range)))
-    n_images = 10
+    n_images = 20
 
     for i, eps in enumerate(eps_range):
-        # eps_losses = []
         for j, step_size in enumerate(step_size_range):
             kwargs = {
                 'eps': eps,
-                'n_iterations': 100,
+                'n_iterations': 200,
                 'step_size': step_size
             }
             hparam = 'eps={},step_size={},n_iterations=100'.format(eps, step_size)
@@ -68,6 +69,7 @@ if __name__ == '__main__':
                 adversarial_label = get_label(adv_img, model)
                 predicted_label = get_label(img, model)
                 if adversarial_label != predicted_label:
+                    # We fooled the classifier
                     count += 1
 
                     # fig, orig_label, adversarial_label = draw_result(img, noise, adv_img, model=model)
@@ -82,49 +84,36 @@ if __name__ == '__main__':
                     # plt.close()
             res[i, j] = count
 
-            final_loss = losses[-1]
-        final_losses.append(final_loss)
+            # final_loss = losses[-1]
+            # final_losses.append(final_loss)
 
-    # Losses after convergence
-    for i, eps in enumerate(eps_range):
-        plt.plot(final_losses[i], step_size_range, label=eps)
-    plt.title('Cross Entropy')
-    plt.ylabel('loss')
-    plt.xlabel('step_size')
-    plt.legend()
-    plt.savefig('out/loss_function_of_step_{}.png'.format(eps))
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.set_title('Percentage of successful adversarial images')
-    plt.imshow(res / n_images, cmap='hot')
-
-    # ax.set_xticks(step_size_range)
-    # plt.xticks([itstep_size_range])
-    # plt.gca().set_xticks(step_size_range)
-    ax.set_xlabel('step size')
-
-    # ax.set_yticks(eps_range)
-    ax.set_ylabel('max noise')
-
-    plt.colorbar()
-    plt.savefig('successful_adv_images.png')
+            # Losses after convergence
+            # for i, eps in enumerate(eps_range):
+            # plt.plot(final_losses[i], step_size_range, label=eps)
+    print(res)
+    perc_res = res / n_images
+    fig = plot_heatmap_results(res=perc_res,
+                               x_range=step_size_range,
+                               y_range=eps_range,
+                               x_label='step size',
+                               y_label='max noise')
+    plt.savefig('out/mnist_successful_adv_images.png')
 
     # Accuracy
 
     # Run non targeted
-    n_images = 50
-    img, label = next(iter(test_loader))
-    count = 0
-    iter_count = 0
-    for img, label in test_loader:
-        iter_count += 1
-        if iter_count > n_images:
-            break
-        adv_img, noise, losses = run_non_targeted_attack(image=img, model=model, **kwargs)
-        adversarial_label = get_label(adv_img, model)
-        predicted_label = get_label(img, model)
-        if adversarial_label == predicted_label:
-            count += 1
-
-    print("Mean accuracy: ", 1.0 * count / n_images)
+    # n_images = 50
+    # img, label = next(iter(test_loader))
+    # count = 0
+    # iter_count = 0
+    # for img, label in test_loader:
+    #     iter_count += 1
+    #     if iter_count > n_images:
+    #         break
+    #     adv_img, noise, losses = run_non_targeted_attack(image=img, model=model, **kwargs)
+    #     adversarial_label = get_label(adv_img, model)
+    #     predicted_label = get_label(img, model)
+    #     if adversarial_label == predicted_label:
+    #         count += 1
+    #
+    # print("Mean accuracy: ", 1.0 * count / n_images)
